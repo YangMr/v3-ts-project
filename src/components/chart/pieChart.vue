cc70147d0b6238aa5673164c0531b6588e843aae
yangmr <759168442@qq.com>
2024-03-14 11:08:29 +0800

    实现销售统计图标

---
+++ src/components/chart/pieChart.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup>
+import * as echarts from 'echarts'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  init()
+})
+
+const init = () => {
+  const chartDom = document.getElementById('chartRef')
+  const myChart = echarts.init(chartDom)
+
+  const option = {
+    title: {
+      text: 'Referer of a Website',
+      subtext: 'Fake Data',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: '50%',
+        data: [
+          { value: 1048, name: 'Search Engine' },
+          { value: 735, name: 'Direct' },
+          { value: 580, name: 'Email' },
+          { value: 484, name: 'Union Ads' },
+          { value: 300, name: 'Video Ads' }
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  }
+
+  option && myChart.setOption(option)
+}
+</script>
+
+<template>
+  <el-card shadow="hover">
+    <div id="chartRef" style="width: 100%; height: 380px"></div>
+  </el-card>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,11 +2,32 @@
 import { defineAsyncComponent } from 'vue'
 
 const StaticBlock = defineAsyncComponent(() => import('./components/staticBlock.vue'))
+const pieChart = defineAsyncComponent(() => import('@/components/chart/pieChart.vue'))
 </script>
 
 <template>
   <div>
+    <!-- 营业数据统计-->
     <StaticBlock></StaticBlock>
+    <!-- 分类销售统计与近30天销售趋势-->
+    <el-row :gutter="20">
+      <el-col :lg="9" :md="11" :sm="24" :xs="24" class="mb15">
+        <!--饼图-->
+        <pieChart></pieChart>
+      </el-col>
+      <el-col :lg="15" :md="13" :sm="24" :xs="24" class="mb15">
+        <el-card shadow="hover">近30天销售趋势</el-card>
+      </el-col>
+    </el-row>
+    <!-- 会员消费与气温仪表盘-->
+    <el-row :gutter="20">
+      <el-col :lg="15" :md="13" :sm="24" :xs="24" class="mb15">
+        <el-card shadow="hover">分类销售统计</el-card>
+      </el-col>
+      <el-col :lg="9" :md="11" :sm="24" :xs="24" class="mb15">
+        <el-card shadow="hover">近30天销售趋势</el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
